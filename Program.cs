dd7c14bf06482c0d30535093accf9923a8475874
JosipNedic12 <josip.nedic.jn@gmail.com>
2024-11-05 11:48:13 +0100

    Dodana početna aplikacija s porukom pozdrava

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PozdravAplikacija
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Josip Nedic");
+        }
+    }
+}
